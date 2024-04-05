d356d889384b88d9c52b5d034412a0400468c831
ybalkas@gmail.com <ybalkas@gmail.com>
2024-04-05 09:30:54 +0100

    Contact us model added

---
+++ Verisk_automation/Models/ContactUsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowProject3.Models
+{
+    internal class ContactUsModel
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string   businessEmail{ get; set; }
+        public string? businessPhone { get; set; }
+        public string? company { get; set; }
+        public string? comments { get; set; }
+    }
+}
