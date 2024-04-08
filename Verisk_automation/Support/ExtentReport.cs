d32caa2a31f2d1e993adb9e5304395767bf65396
ybalkas@gmail.com <ybalkas@gmail.com>
2024-04-08 03:17:43 +0100

    Extent reports added

---
+++ Verisk_automation/Support/ExtentReport.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter.Config;
+using AventStack.ExtentReports.Reporter;
+
+namespace Verisk_automation.Support
+{
+
+    public class ExtentReport
+    {
+        public static ExtentReports _extentReports;
+        public static ExtentTest _feature;
+        public static ExtentTest _scenario;
+
+        public static String dir = AppDomain.CurrentDomain.BaseDirectory;
+        public static String testResultPath = dir.Replace("bin\\Debug\\net6.0", "TestResults");
+
+        public static void ExtentReportInit()
+        {
+            //ExtentSparkReporter htmlReporter = new ExtentSparkReporter("Spark.html");
+            ExtentSparkReporter htmlReporter = new ExtentSparkReporter(testResultPath+"\\spark.html");
+            //var htmlReporter = new ExtentHtmlReporter(testResultPath);
+            htmlReporter.Config.ReportName = "Automation Status Report";
+            htmlReporter.Config.DocumentTitle = "Automation Status Report";
+            htmlReporter.Config.Theme = Theme.Standard;
+            //htmlReporter.Start();
+
+            _extentReports = new ExtentReports();
+            _extentReports.AttachReporter(htmlReporter);
+            _extentReports.AddSystemInfo("Application", "Verisk");
+            _extentReports.AddSystemInfo("Browser", "Chrome");
+            _extentReports.AddSystemInfo("OS", "Windows");
+        }
+
+        public static void ExtentReportTearDown()
+        {
+            _extentReports.Flush();
+        }
+
+        public string addScreenshot(IWebDriver driver, ScenarioContext scenarioContext)
+        {
+            ITakesScreenshot takesScreenshot = (ITakesScreenshot)driver;
+            Screenshot screenshot = takesScreenshot.GetScreenshot();
+            string screenshotLocation = Path.Combine(testResultPath, scenarioContext.ScenarioInfo.Title + ".png");
+            screenshot.SaveAsFile(screenshotLocation);
+            return screenshotLocation;
+        }
+
+
+    }
+}
